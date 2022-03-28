2cdf9759841384fba81c15aa10f139fc79f28a1d
FreeMoneyHyb <83422129+FreeMoneyHyb@users.noreply.github.com>
2022-03-28 12:56:02 -0500

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -5,12 +5,12 @@ import random
 print('''
 
 Webhook Nuker
-v1.1.5 FMH
+v1.2.0 FMH
 
 1 = hentai hook (and normal)
 2 = jibberish spam
 3 = hellish spam (ruins discord)
-4 = nitro spam (lags servers)
+4 = fake gw spam (lags servers)
 ''')
 maaain = input("Enter Number : ")
 if maaain =="1":
@@ -78,7 +78,18 @@ elif maaain =="3":
        print(f"Rate Limited {h}")
     if h.status_code == 400:
       print("More Then 200 Caracters Dectected!")
-       
+elif maaain =="4":
+  name7 = "discord"
+  webhook = input("Enter The Entire Discord Webhook : ")
+  ee = int(999999900)
+  for i in range(ee):
+    cood43 = "".join(random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")for _ in range(16))        
+    message = f"@everyone you won **lucky webhook week** and got **FREE nitro premium** discord.gift/{cood43} learn more about discord.gift/winners on the offical discord site https://media.discordapp.net/attachments/958026963581300768/958058952510283837/9cca3b40-e1f0-4d27-b42c-d8897b6cf95d.gif discord.gift/tos123 for more giveaways "
+    h = requests.post(webhook, json={"content": str(message), "name": str(name7), "avatar_url": "https://cdn.discordapp.com/attachments/933540730083872889/935019209652596736/01d8Ghah.jpg"})
+    print(f"Message Sent - {h}")
+    if h.status_code == 429:    
+       print(f"Rate Limited {h}")
+    if h.status_code == 400:
+      print("More Then 200 Caracters Dectected!")    
 else:
   print("Invalid Responce")
-
